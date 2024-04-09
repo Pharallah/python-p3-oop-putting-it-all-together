faaee4ee9a65e46a19f59a5499c163543ec04854
Harry San Juan <harrycsanjuan@gmail.com>
2024-04-09 16:51:20 -0400

    completed lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, condition=None):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+
+
+        
+
+stan_smith = Shoe("Adidas", 9)
+
